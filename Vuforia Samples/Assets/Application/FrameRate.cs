2dc39881863db5d1fe287430fcf2679910ebb22f
Nicholas Maurer <nicholasallenmaurer@gmail.com>
2019-11-28 14:21:44 +1100

    added vuforia project

---
+++ Vuforia Samples/Assets/Application/FrameRate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class FrameRate : MonoBehaviour
+{
+    private float deltaTime = 0;
+    private float msec = 0;
+    private float fps = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        deltaTime += (Time.unscaledDeltaTime - deltaTime) * 0.1f;
+        msec = deltaTime * 1000.0f;
+        fps = 1.0f / deltaTime;
+    }
+
+    private void OnGUI()
+    {
+        GUILayout.Label(string.Format("fps: {0}", fps));
+    }
+}
---
+++ Vuforia Samples/Assets/Application/TestSample.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSample : MonoBehaviour
+{
+    public GameObject modelGo;
+
+    public GameObject cameraGo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+//        GUILayout.Label(string.Format("Camera Position {0}", cameraGo.transform.position));
+//        GUILayout.Label(string.Format("Model Position {0}", modelGo.transform.position));
+    }
+
+    private void OnDrawGizmos()
+    {
+       Gizmos.DrawLine(cameraGo.transform.position, modelGo.transform.position);
+    }
+}
